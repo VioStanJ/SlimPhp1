aaca9df2c690c6f422d27e24c4daea6a08f21bde
viostanj <viostanojguerrier@gmail.com>
2019-07-14 16:09:37 -0400

    adding db conf on ignore folder

---
+++ src/config/.gitignore/data.php
@@ -0,0 +1,5 @@
+<?php
+$dbname = 'cloudcontact';
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
--- src/config/db.php
+++ src/config/db.php
@@ -1,10 +1,8 @@
 <?php
 
+require '.gitignore/data.php';
+
 class DB{
-    private $dbname = 'cloudcontact';
-    private $dbhost = 'localhost';
-    private $dbuser = 'root';
-    private $dbpass = '';
 
     static function connect(){
         $pdo = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname,
@@ -12,7 +10,7 @@ class DB{
 
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        
+
         return $pdo;
     }
 }
